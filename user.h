0f2574ea70faf8a951d201fdd797999a33d79dd4
mrmohammadsh <mr.mohammad13982000@gmail.com>
2019-01-19 13:21:51 +0330

    change in user.h for pass to mostafa

--- user.h
+++ user.h
@@ -13,36 +13,89 @@
 
 using namespace std;
 
-void userMain(int username, int accountNumber)
+void userMain(int username, int account_Number, int password)
 {
-    cout << "Enter the number of task you want to do please\n";
-    cout << "1. Buy ticket\n";
-    cout << "2. Cancel ticket\n";
-    cout << "3. See history of tickets\n";
-    cout << "0. Exit\n";
-    int menuNumber;
-    cin >> menuNumber;
-    if(menuNumber == 1)
-    {
-        //goes to function for buy ticket
+	cout << "Enter the number of task you want to do please\n";
+	cout << "1. Buy ticket\n";
+	cout << "2. Cancel ticket\n";
+	cout << "3. See history of tickets\n";
+	cout << "0. Exit\n";
+	int menuNumber;
+	cin >> menuNumber;
+	if (menuNumber == 1)
+	{
+		userTicket(username, account_Number, password);
 
-    }
-    else if(menuNumber == 2)
-    {
-        //goes to function cancel ticket for user
-    }
-    else if(menuNumber == 3)
-    {
-        //goes to finction to see history of tickets
-    }
-    else if(menuNumber == 0)
-    {
-        return;
-    }
-    else
-    {
-        cout << "Sorry the number you have entered is not correct!! Try again please..\n\n";
-        return;
-    }
+	}
+	else if (menuNumber == 2)
+	{
+		//goes to function cancel ticket for user
+	}
+	else if (menuNumber == 3)
+	{
+		//goes to finction to see history of tickets
+	}
+	else if (menuNumber == 0)
+	{
+		return;
+	}
+	else
+	{
+		cout << "Sorry the number you have entered is not correct!! Try again please..\n\n";
+		return;
+	}
 
 }
+
+
+
+void book(int username, int account_Number, int password)
+{
+	int i, r;
+	FILE * allUsersFile = fopen("allUsers.txt", "r");
+	if (allUsersFile == NULL)
+	{
+		cout << "The File opening was Unsuccessful!\n";
+		return;
+	}
+	int userName_temp, pass_temp, accountNumber;
+	char firstName[21], lastName[21], phone[21], reagent[21];
+	for (i = 0; i<100000; i++)
+	{
+		fscanf(allUsersFile, "%d\t%d\t%d\t%s\t%s\t%s\t%s\n", &userName_temp, &pass_temp, &accountNumber, firstName, lastName, phone, reagent);
+		if (username == userName_temp)
+		{
+			if (password == pass_temp)
+			{
+
+
+
+
+
+
+
+			}
+			else
+			{
+				cout << "The password is incorrect!\n";
+				return;
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
